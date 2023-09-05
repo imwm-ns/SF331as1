f278911f336d9ff2f92f9d352991a0f0dd11a95d
NarathipDev <narathip_aot@hotmail.com>
2023-09-05 18:31:17 +0700

    Add Header.

--- app/src/main/java/com/example/sf333as1/MainActivity.kt
+++ app/src/main/java/com/example/sf333as1/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.sf333as1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sf333as1.ui.theme.SF333as1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +26,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SF333as1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GuessNumber()
                 }
             }
         }
@@ -30,17 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GuessNumber() {
+    Column {
+        Text(
+            text = stringResource(R.string.header),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Blue, )
+                .padding(vertical = 15.dp, horizontal = 10.dp),
+            color = Color.White
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GuessNumberPreview() {
     SF333as1Theme {
-        Greeting("Android")
+        GuessNumber()
     }
 }
